af73778d8e227400344145678ea91f0ef9e26280
alexpsimone <alex.p.simone@gmail.com>
2020-11-04 22:03:13 +0000

    added /procedure route

--- server.py
+++ server.py
@@ -8,5 +8,11 @@ def show_homepage():
 
     return render_template('homepage.html')
 
+@app.route('procedure/')
+def show_homepage():
+    """Render a procedure page."""
+
+    return render_template('procedure.html')
+
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
