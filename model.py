a03e558735e181bb525890f584243140e474fec8
alexpsimone <alex.p.simone@gmail.com>
2020-11-05 20:22:11 +0000

    Added function and call to connect to a database called shopcat, so I can test in Python interactive mode.

--- model.py
+++ model.py
@@ -58,3 +58,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User user_id={self.user_id} username={self.username}>'
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///shopcat', echo = True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to Shop Cat database!')
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
