acb0c57d03e783eaf75b105f08d3194163de700b
alexpsimone <alex.p.simone@gmail.com>
2020-11-10 00:40:50 +0000

    added ajax event handler to add a tool to the form upon button click

--- static/js/write-procedure.jsx
+++ static/js/write-procedure.jsx
@@ -1,1 +1,15 @@
 'use strict';
+
+document.querySelector('#tool-adder').addEventListener('click', (evt) => {
+    document.querySelector('#tool-list').insertAdjacentHTML('beforeend', 
+        '<br /><label>Tool Required: </label>'
+        + '<select name="tool_req">'
+        + '<option value="">--Please select a tool--</option>'
+        + '{% for tool in tools %}'
+        + '<option value="{{ tool.name }}">{{ tool.name }}</option>'
+        + '{% endfor %}'
+        + '<option value="other">Other (please specify)...</option>'
+        + '</select>'
+        + '<br /><label>If other, please specify: </label>'
+        + '<input type="text" name="tool_other" /><br />');
+});
