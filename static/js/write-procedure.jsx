eb32ddab9a8bb56a78946e242b8224fea73592db
alexpsimone <alex.p.simone@gmail.com>
2020-11-10 23:28:42 +0000

    use jQuery and hidden number inputs to return NUM_TOOLS and NUM_PARTS to server with form submission

--- static/js/write-procedure.jsx
+++ static/js/write-procedure.jsx
@@ -6,29 +6,36 @@ let NUM_PARTS = 0;
 function getTools (evt) {
 
     evt.preventDefault();
+    
 
     $.get('/get-tools.json', (res) => { 
-
+        
         NUM_TOOLS += 1;
         let str = '';
-
+        
         for (const tool of res) {
             str = str + `<option value="${tool}">${tool}</option>`;
         }
 
         $('#tool-list').append(
             `<br />
-            <label>Tool Required: </label>
+            <label>Tool #${NUM_TOOLS}: </label>
             <select name="tool_req" class="tool-req" id="tool${NUM_TOOLS}">
             <option value="">--Please select a tool--</option>
             ${str}
             <option value="other">Other (please specify)...</option>
             </select>
             <br /><label>If other, please specify: </label>
-            <input type="text" name="tool_other" />`
+            <input type="text" name="tool_other" />
+            <br />`
         );
 
-    });   
+        $('#NUM_TOOLS').replaceWith(`<input id="NUM_TOOLS" type="number" 
+            value="${NUM_TOOLS}" style="display: none;"/>`);
+
+    });
+    
+    
 }
 
 
@@ -47,15 +54,19 @@ function getParts (evt) {
 
         $('#part-list').append(
             `<br />
-            <label>Part Required: </label>
+            <label>Part #${NUM_PARTS}: </label>
             <select name="part_req" class="part-req" id="part${NUM_PARTS}">
             <option value="">--Please select a part--</option>
             ${str}
             <option value="other">Other (please specify)...</option>
             </select>
             <br /><label>If other, please specify: </label>
-            <input type="text" name="part_other" />`
+            <input type="text" name="part_other" />
+            <br />`
         );
+
+        $('#NUM_PARTS').replaceWith(`<input id="NUM_PARTS" type="number" 
+            value="${NUM_PARTS}" style="display: none;"/>`);
 
     });   
 }
