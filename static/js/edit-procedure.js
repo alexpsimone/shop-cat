ade5d1be504f577acdb771fab8bb8a3eae7129a2
alexpsimone <alex.p.simone@gmail.com>
2020-11-18 03:35:50 +0000

    num_tools updates properly with tool addition/removal (DOM only)

--- static/js/edit-procedure.js
+++ static/js/edit-procedure.js
@@ -12,20 +12,60 @@ $('#label-remove').on('change', () => {
 });
 
 
-
 function removeTool (evt) {
 
     evt.preventDefault();
 
     const toolButton = evt.target;
-
     const toolName = toolButton['value'];
-    console.log(`toolName: ${toolName}`)
 
-     $(`#row-${toolName}`).remove();
+    $(`#row-${toolName}`).remove();
+
+    let NUM_TOOLS = Number($('#NUM_TOOLS').val());
+    NUM_TOOLS -= 1;
+
+    $('#NUM_TOOLS').replaceWith(`<input name="NUM_TOOLS" id="NUM_TOOLS" 
+                                    type="hidden" value="${NUM_TOOLS}" />`);
 }
 
 $('button.remove-tool').on('click', removeTool);
+
+
+function addTool (evt) {
+
+    evt.preventDefault();
+    
+    let NUM_TOOLS = Number($('#NUM_TOOLS').val());
+    NUM_TOOLS += 1;
+
+    $.get('/get-tools.json', (res) => { 
+        
+        let str = '';
+        for (const tool of res) {
+            str = str + `<option value="${tool}">${tool}</option>`;
+        }
+
+        $('#tools').append(
+            `<br />
+            <label>New Tool: </label>
+            <select name="tool_req_${NUM_TOOLS}" class="tool-req" id="tool${NUM_TOOLS}">
+            <option value="">--Please select a tool--</option>
+            ${str}
+            <option value="other">Other (please specify)...</option>
+            </select>
+            <br /><label>If other, please specify: </label>
+            <input type="text" name="tool_other_${NUM_TOOLS}" />
+            <br />`
+        );
+
+    });
+
+    $('#NUM_TOOLS').replaceWith(`<input name="NUM_TOOLS" id="NUM_TOOLS" 
+                                    type="hidden" value="${NUM_TOOLS}" />`);
+    
+}
+
+$('#tool-add').on('click', addTool);
 
 
 function addVehicle (evt) {
