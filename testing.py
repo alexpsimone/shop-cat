06aace9e94f713eae7fc10241d663b2e522a8033
alexpsimone <alex.p.simone@gmail.com>
2020-11-24 23:45:37 +0000

    moved test_db creation/seeding to flask test setUp func, which fixed some testing issues; also updated route coverage list at top of file

--- testing.py
+++ testing.py
@@ -7,25 +7,34 @@ Routes covered by this file:
 /write-procedure
 
 Routes not fully covered by this file:
-/new-user
+/build-procedure
+/dashboard/<user_id>
+/edit-procedure/<proc_id>
 /existing-user
 /get-models.json
-/get-tools.json
 /get-parts.json
+/get-tools.json
+/new-user
+/rebuild-procedure
+/tool/<tool_id>
+/uploads/<filename>
+/vehicle/<make>
+/vehicle/<make>/<model_year>
+/vehicle/<make>/<model_year>/<model>
 /vehicle-select
 /vehicle-select.json
-/build-procedure
 """
 
 import unittest
 from server import app
-from model import db, connect_to_db, Procedure, User
+from model import db, connect_to_db, User, Procedure, Car, Part, Tool, Step
+from model import PartNum, ProcedureCar, ProcedurePart, ProcedureTool
 import os
 
 # Drop and re-create the test database.
-os.system("dropdb testdb")
-os.system("createdb testdb")
-os.system("python3 seed_testdb.py")
+# os.system("dropdb testdb")
+# os.system("createdb testdb")
+# os.system("python3 seed_testdb.py")
 
 
 class FlaskTests(unittest.TestCase):
@@ -60,13 +69,18 @@ class ShopCatTestsDatabase(unittest.TestCase):
     def setUp(self):
         """Stuff to do before every test."""
 
-        self.client = app.test_client()
         app.config["TESTING"] = True
+        app.config['SECRET_KEY'] = 'key'
+        self.client = app.test_client()
 
         # Connect to test database
+        os.system("dropdb testdb")
+        os.system("createdb testdb")
+        os.system("python3 seed_testdb.py")
         connect_to_db(app, db_uri="postgresql:///testdb")
-        db.create_all()
+        # db.create_all()
 
+        # Put user1 into session.
         with self.client as c:
             with c.session_transaction() as sess:
                 sess["current_user"] = 1
@@ -87,9 +101,9 @@ class ShopCatTestsDatabase(unittest.TestCase):
     def test_write_procedure_route(self):
         """Check that the write-procedure route is rendering properly."""
 
-        result = self.client.get("/write-procedure")
+        result = self.client.get("/write-procedure", follow_redirects=True)
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'<form action="/build-procedure", method="POST">', result.data)
+        self.assertIn(b'<form action="/build-procedure", method="POST", enctype="multipart/form-data">', result.data)
 
     def test_procedure_by_proc_id_route(self):
         """Check that the procedure view route is rendering properly."""
@@ -99,7 +113,7 @@ class ShopCatTestsDatabase(unittest.TestCase):
         for procedure in all_procedures:
             result = self.client.get(f"/procedure/{procedure.proc_id}")
             self.assertEqual(result.status_code, 200)
-            self.assertIn(f"<h1>{procedure.title}</h1>", result.data)
+            self.assertIn(b"<p>Here are the steps in the procedure:</p>", result.data)
             self.assertNotIn(b'<form action="/vehicle-select"', result.data)
 
     def test_new_user_route_new(self):
@@ -155,7 +169,6 @@ class ShopCatTestsDatabase(unittest.TestCase):
         )
         self.assertEqual(result.status_code, 200)
         # self.assertIn(b'Browse all the available procedures:', result.data)
-
 
 if __name__ == "__main__":
     unittest.main()
